32c68c8ca8a838d00eb4f0ac32a2d3145ca10818
MartinPa <99693089+MartinPans@users.noreply.github.com>
2023-09-24 00:07:08 +0200

    Slider

---
+++ RDVHUnity/Assets/HUD/CounterScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CounterScript : MonoBehaviour
+{
+    [SerializeField] Slider repSlider;
+    [SerializeField] TextMeshProUGUI moneyCounter;
+
+    private void Start()
+    {
+        repSlider.maxValue = 20;
+        repSlider.value = 0;
+
+    }
+
+    private void Update()
+    {
+        repSlider.value = Lib.instance.reputationCounter;
+        moneyCounter.text = Lib.instance.moneyCounter.ToString();
+    }
+}
