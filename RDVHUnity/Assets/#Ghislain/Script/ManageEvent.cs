fa35a9ac8f285840ebf551810ab705fd28e87106
Ghislain Thiery <ghislain.thiery@ntymail.com>
2023-09-23 23:27:51 +0200

    Debut d'integration UI

--- RDVHUnity/Assets/#Ghislain/Script/ManageEvent.cs
+++ RDVHUnity/Assets/#Ghislain/Script/ManageEvent.cs
@@ -17,12 +17,49 @@ public class ManageEvent : MonoBehaviour
     [SerializeField] TextMeshProUGUI _choice1;
     [SerializeField] TextMeshProUGUI _choice2;
 
+    [SerializeField] GameObject _button1;
+    [SerializeField] GameObject _button2;
     [SerializeField] GameObject _gameManager;
 
     private int changeToMoney1;
     private int changeToReputation1;    
     private int changeToMoney2;
     private int changeToReputation2;
+
+    public void NewEvent()
+    {
+        //Get turn
+        int turn = Lib.instance.semesterCounter;
+
+        //Set the range
+        int minRange = 0;
+        int maxRange = 0;
+        switch (turn)
+        {
+            case 0:
+                minRange = 0;
+                maxRange = 0;
+                break;
+            case 1:
+                minRange = 1;
+                maxRange = 1;
+                break;
+            case 2:
+                minRange = 2;
+                maxRange = 2;
+                break;
+            case > 3:
+                minRange = 0;
+                maxRange = 0;
+                break;
+            default:
+                break;
+        }
+        //...and generate random event
+        GenerateEvent(minRange, maxRange);
+
+    }
+
 
     public void GenerateEvent(int minRange, int maxRange)
     {
@@ -37,6 +74,10 @@ public class ManageEvent : MonoBehaviour
         UpdateEventPanel(_previousEventIndex);
 
         //play event sound
+        if (_events[_previousEventIndex].Sound != null)
+        {
+            SoundManager.Instance.PlaySound(_events[_previousEventIndex].Sound);
+        }
     }
 
     public void UpdateEventPanel(int index)
@@ -45,13 +86,38 @@ public class ManageEvent : MonoBehaviour
         _textArea.text = _events[index].Texte;
         _imageArea.sprite = _events[index].Image;
 
-        _choice1.text = _events[index].TexteChoix1;
-        changeToMoney1 = _events[index].ChangeToMoney1;
-        changeToReputation1 = _events[index].ChangeToReputation1;
 
-        _choice2.text = _events[index].TexteChoix2;
-        changeToMoney2 = _events[index].ChangeToMoney2;
-        changeToReputation2 = _events[index].ChangeToReputation2;
+        if (_events[index].TexteChoix2 == "")
+        {
+            _button2.SetActive(false);
+        }
+        else
+        {
+            _button2.SetActive(true);
+            _choice2.text = _events[index].TexteChoix2;
+            changeToMoney2 = _events[index].ChangeToMoney2;
+            changeToReputation2 = _events[index].ChangeToReputation2;
+        }
+
+
+        if (_events[index].TexteChoix1 == "")
+        {
+            if (_events[index].TexteChoix2 == "")
+            {
+                _choice1.text = "Suivant";
+                changeToMoney1 = 0;
+                changeToReputation1 = 0;
+            }
+            else _button1.SetActive(false);
+        }
+        else
+        {
+            _button1.SetActive(true);
+            _choice1.text = _events[index].TexteChoix1;
+            changeToMoney1 = _events[index].ChangeToMoney1;
+            changeToReputation1 = _events[index].ChangeToReputation1;
+        }
+
     }
 
 
@@ -62,10 +128,7 @@ public class ManageEvent : MonoBehaviour
         Lib.instance.SetMoney(changeToMoney1);
         Lib.instance.SetReputation(changeToReputation1);
 
-        //Update the counters
-        _gameManager.GetComponent<ManageCounters>().UpdateCounters();
-
-        gameObject.SetActive(false);
+        OnChoosingAnswer();
     }
 
     public void ChooseNumberTwo()
@@ -75,9 +138,16 @@ public class ManageEvent : MonoBehaviour
         Lib.instance.SetMoney(changeToMoney2);
         Lib.instance.SetReputation(changeToReputation2);
 
+        OnChoosingAnswer();
+    }
+
+    private void OnChoosingAnswer()
+    {
         //Update the counters
         _gameManager.GetComponent<ManageCounters>().UpdateCounters();
-
+        //Update semester count
+        Lib.instance.semesterCounter += 1;
+        //disable panel
         gameObject.SetActive(false);
     }
 
