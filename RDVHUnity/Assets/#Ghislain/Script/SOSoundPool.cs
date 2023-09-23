7e163bdf4c671398a20cf4e49f0de56232da7663
Ghislain Thiery <ghislain.thiery@ntymail.com>
2023-09-23 09:09:36 +0200

    Audio manager

---
+++ RDVHUnity/Assets/#Ghislain/Script/SOSoundPool.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundPool", menuName = "Scriptable object/Sound Pool")]
+public class SOSoundPool : ScriptableObject
+{
+    [SerializeField] AudioClip[] _sounds;
+
+    //Properties
+    private int currentSound { get; set; }
+
+    //Methods
+
+    public void PlaySound(AudioSource source)
+    {
+        //Debug if there is no sound in the pool
+        if (_sounds.Length < 1)
+        {
+            Debug.Log("Attention : pas d'audioclip dans la sound pool");
+            return;
+        }
+        //Choose random sound
+        int index = Random.Range(0, _sounds.Length);
+
+        //Play it
+        source.clip = _sounds[index];
+        source.PlayOneShot(source.clip);
+    }
+    
+    public void PlayMusic(AudioSource source)
+    {
+        if (!source.loop)
+        {
+            source.loop = true;
+        }
+        source.clip = _sounds[0];
+        source.Play();
+    }
+
+}
