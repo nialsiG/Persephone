5152ea6c1b6e889df5961201c6ee231a4b3ddb3e
MartinPa <99693089+MartinPans@users.noreply.github.com>
2023-09-24 11:38:11 +0200

    AnimAsset

---
+++ RDVHUnity/Assets/IABehaviour/RdmMvt.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class RdmMvt : MonoBehaviour
+{
+    [SerializeField] Vector2 limitHor, limitVer;
+    public float counter = -1, speed;
+    private Vector3 dir;
+
+    private bool changeDir;
+
+    private void Update()
+    {
+        if (counter < 0)
+        {
+            if (!changeDir && transform.position.x > limitHor.x && transform.position.x < limitHor.y && transform.position.y > limitVer.x && transform.position.x < limitVer.y)
+            {
+                dir = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0);
+                dir.Normalize();
+                changeDir = true;
+
+                StartCoroutine(Stop());
+                
+            }
+            else
+            {
+                dir = Vector3.zero - transform.position;
+                dir.Normalize();
+            }
+
+
+        }
+        else
+            counter -= Time.deltaTime;
+
+        GetComponent<SpriteRenderer>().flipX = (dir.x > 0);
+
+
+        if (!changeDir)
+            transform.Translate(dir * speed * Time.deltaTime);
+        else
+        {
+
+        }
+    }
+
+    IEnumerator Stop()
+    {
+        yield return new WaitForSeconds(Random.Range(2, 3.5f));
+        changeDir = false;
+        counter = Random.Range(2, 5);
+    }
+}
