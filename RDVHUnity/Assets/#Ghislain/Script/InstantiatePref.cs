e6a611cb76c12f6029597f8858f73848527950c7
MartinPa <99693089+MartinPans@users.noreply.github.com>
2023-09-24 16:54:18 +0200

    Last

--- RDVHUnity/Assets/#Ghislain/Script/InstantiatePref.cs
+++ RDVHUnity/Assets/#Ghislain/Script/InstantiatePref.cs
@@ -4,7 +4,7 @@ public class InstantiatePref : MonoBehaviour
 {
     [SerializeField] GameObject GO = null;
     [SerializeField] Transform chapelleTransform = null, cabaneTransform = null;
-
+    
     public void InstantiateOnMouse()
     {
         Lib.instance.CurrentObject = Instantiate(GO, transform.position, Quaternion.identity);
--- RDVHUnity/Assets/HUD/CounterScript.cs
+++ RDVHUnity/Assets/HUD/CounterScript.cs
@@ -7,6 +7,8 @@ public class CounterScript : MonoBehaviour
     [SerializeField] Slider repSlider;
     [SerializeField] TextMeshProUGUI moneyCounter;
 
+    [SerializeField] GameObject Vict, defaite;
+
     private void Start()
     {
         repSlider.maxValue = 20;
@@ -16,6 +18,16 @@ public class CounterScript : MonoBehaviour
 
     private void Update()
     {
+        if (Lib.instance.moneyCounter >= 3001)
+        {
+            Vict.SetActive(true);
+        }
+
+        if (Lib.instance.moneyCounter < 0)
+        {
+            defaite.SetActive(true);
+        }
+
         repSlider.value = Lib.instance.reputationCounter;
         moneyCounter.text = Lib.instance.moneyCounter.ToString();
     }
